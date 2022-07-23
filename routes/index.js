3011b9e5f759d08c26359e9d0ac9eb420d433b11
Steerpike <imthekoul@gmail.com>
2022-07-23 21:26:11 +0300

    Form && post method

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
-  },
-];
+const messages = require('../public/javascripts/messages');
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Mini Messageboard', messages });
--- routes/new.js
+++ routes/new.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const messages = require('../public/javascripts/messages');
 
 router.get('/', function (req, res, next) {
   res.render('new', { title: 'New message' });
+});
+
+router.post('/', (req, res) => {
+  if (!req.body.messageArea || !req.body.userName) {
+    res.status(400).send('Entries my have a title and a body');
+    return;
+  }
+  messages.push({
+    text: req.body.messageArea,
+    user: req.body.userName,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
