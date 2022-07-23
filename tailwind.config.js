1e057f4be3d282c63b11c129221000e90f578692
Steerpike <imthekoul@gmail.com>
2022-07-23 17:22:28 +0300

    Created app with express generator -ejs template and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{ html,js,css}', './views/*.ejs', './views/partial/*.ejs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
